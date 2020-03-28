989a2d91c150acf374c78f2cde436ad8e17be768
PRIYANSHUTIWARI9 <priyanshu.tiwari_cs18@gla.ac.in>
2020-03-28 11:19:53 +0530

    complete definition of tree node

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -7,4 +7,20 @@
 package problem1.mybst;
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+    private TreeNode root;
+    private ArrayList<Integer> arrayList;
+
+    public MyBinarySearchTree() {
+        arrayList = new ArrayList<>();
+    }
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(TreeNode root) {
+        this.root = root;
+    }
+
+
 }
